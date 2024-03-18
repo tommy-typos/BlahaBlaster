477930c17311052ffb9cd8349f0f7b14b63b2974
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-18 23:00:47 +0100

    add key handler to listen to users' actions

---
+++ src/KeyHandler.java
@@ -0,0 +1,68 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener{
+    public boolean w, a, s, d, up, down, left, right;
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_W){
+            w = true;
+        }
+        if(key == KeyEvent.VK_A){
+            a = true;
+        }
+        if(key == KeyEvent.VK_S){
+            s = true;
+        }
+        if(key == KeyEvent.VK_D){
+            d = true;
+        }
+        if(key == KeyEvent.VK_UP){
+            up = true;
+        }
+        if(key == KeyEvent.VK_DOWN){
+            down = true;
+        }
+        if(key == KeyEvent.VK_LEFT){
+            left = true;
+        }
+        if(key == KeyEvent.VK_RIGHT){
+            right = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_W){
+            w = false;
+        }
+        if(key == KeyEvent.VK_A){
+            a = false;
+        }
+        if(key == KeyEvent.VK_S){
+            s = false;
+        }
+        if(key == KeyEvent.VK_D){
+            d = false;
+        }
+        if(key == KeyEvent.VK_UP){
+            up = false;
+        }
+        if(key == KeyEvent.VK_DOWN){
+            down = false;
+        }
+        if(key == KeyEvent.VK_LEFT){
+            left = false;
+        }
+        if(key == KeyEvent.VK_RIGHT){
+            right = false;
+        }
+    }
+}
