63c347af7dff9877e5383320ec8bab0f45322870
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-21 09:30:30 +0100

    add tile and tile manager classes

---
+++ src/entity/TileManager.java
@@ -0,0 +1,50 @@
+package entity;
+
+import gui.Game;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class TileManager {
+
+    Game game;
+    GameMap gameMap;
+    public HashMap <String, Tile> tile;
+
+    public TileManager(Game game, GameMap gameMap){
+        this.game = game;
+        tile = new HashMap<>();
+        this.gameMap = gameMap;
+        getTileImages();
+    }
+
+    public void getTileImages(){
+        try{
+            tile.put("grass", new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png")), false));
+
+            tile.put("wall", new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png")), true));
+
+            tile.put("brick", new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/brick.png")), true));
+
+            tile.put("box", new Tile(ImageIO.read(getClass().getResourceAsStream("/tiles/box.png")), true));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isTileCollision(String tileType){
+        return tile.get(tileType).collision;
+    }
+
+    public void draw(Graphics2D g2d){
+        for(int row = 0; row < gameMap.mapDimensions[0]; row++){
+            for(int col = 0; col < gameMap.mapDimensions[1]; col++){
+                BufferedImage img = tile.get(gameMap.mapCells[row][col]).image;
+                g2d.drawImage(img, col * game.tileSize, row * game.tileSize, game.tileSize, game.tileSize, null);
+            }
+        }
+    }
+}
