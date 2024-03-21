2ea90fafb51f4989567fa90737141a5bf467d3f3
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-21 10:22:52 +0100

    fix the collisions

--- src/entity/CollisionChecker.java
+++ src/entity/CollisionChecker.java
@@ -26,31 +26,23 @@ public class CollisionChecker {
         switch (entity.direction){
             case "up":
                 topRow = (entityTopY - entity.speed) / game.tileSize;
-                tile1 = game.gameMap.mapCells[leftCol][topRow];
-                tile2 = game.gameMap.mapCells[rightCol][topRow];
-                System.out.println("leftCol: " + leftCol + " topRow: " + topRow);
-                System.out.println("rightCol: " + rightCol + " topRow: " + topRow);
+                tile1 = game.gameMap.mapCells[topRow][leftCol];
+                tile2 = game.gameMap.mapCells[topRow][rightCol];
                 break;
             case "down":
                 bottomRow = (entityBottomY + entity.speed) / game.tileSize;
-                tile1 = game.gameMap.mapCells[leftCol][bottomRow];
-                tile2 = game.gameMap.mapCells[rightCol][bottomRow];
-                System.out.println("leftCol: " + leftCol + " bottomRow: " + bottomRow);
-                System.out.println("rightCol: " + rightCol + " bottomRow: " + bottomRow);
+                tile1 = game.gameMap.mapCells[bottomRow][leftCol];
+                tile2 = game.gameMap.mapCells[bottomRow][rightCol];
                 break;
             case "left":
                 leftCol = (entityLeftX - entity.speed) / game.tileSize;
-                tile1 = game.gameMap.mapCells[leftCol][topRow];
-                tile2 = game.gameMap.mapCells[leftCol][bottomRow];
-                System.out.println("leftCol: " + leftCol + " topRow: " + topRow);
-                System.out.println("leftCol: " + leftCol + " bottomRow: " + bottomRow);
+                tile1 = game.gameMap.mapCells[topRow][leftCol];
+                tile2 = game.gameMap.mapCells[bottomRow][leftCol];
                 break;
             case "right":
                 rightCol = (entityRightX + entity.speed) / game.tileSize;
-                tile1 = game.gameMap.mapCells[rightCol][topRow];
-                tile2 = game.gameMap.mapCells[rightCol][bottomRow];
-                System.out.println("rightCol: " + rightCol + " topRow: " + topRow);
-                System.out.println("rightCol: " + rightCol + " bottomRow: " + bottomRow);
+                tile1 = game.gameMap.mapCells[topRow][rightCol];
+                tile2 = game.gameMap.mapCells[bottomRow][rightCol];
                 break;
         }
         checkCollision(tile1, tile2, entity);
@@ -58,11 +50,5 @@ public class CollisionChecker {
 
     private void checkCollision(String tile1, String tile2, Entity entity){
         entity.collisionOn = game.tileManager.isTileCollision(tile1) || game.tileManager.isTileCollision(tile2);
-        if(entity.collisionOn){
-            System.out.println("Collision detected " + entity.direction + " " + tile1 + " " + tile2);
-        }else{
-            System.out.println("No collision detected " + entity.direction + " " + tile1 + " " + tile2);
-        }
-
     }
 }
