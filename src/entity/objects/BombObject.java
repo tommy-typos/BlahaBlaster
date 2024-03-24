5b694f94bfaaaff5a5aafa0721dd84b332befcb1
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-24 23:18:27 +0100

    add owner to the bomb

--- src/entity/objects/BombObject.java
+++ src/entity/objects/BombObject.java
@@ -8,9 +8,15 @@ import java.io.IOException;
 
 public class BombObject extends SuperObject{
 
-    public BombObject(Point position, Game game) {
+    public String owner;
+    public Long blowTime = System.currentTimeMillis() + 3000;
+
+    public int blowRadius = 1;
+
+    public BombObject(Point position, Game game, String owner) {
         super(position, game);
         this.name = "bomb";
+        this.owner = owner;
         try{
             this.image = ImageIO.read(getClass().getResourceAsStream("/objects/bomb.png"));
         }catch (IOException e){
