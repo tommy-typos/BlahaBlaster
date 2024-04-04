664782ab6b2cce43e317c15990e7c448254818ab
toghrul <137985975+tommy-typos@users.noreply.github.com>
2024-04-04 08:58:31 +0200

    change start screen

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         ScreenNavigator navigator = new ScreenNavigator(frame, mainPanel);
 
         // ********************* Main Menu Screen *************************
-//        navigator.goto_screen_mainMenu();
+        navigator.goto_screen_mainMenu();
 
         // *********************** Game Info Screen **********************
 //        navigator.goto_screen_gameInfo();
@@ -29,7 +29,7 @@ public class Main {
 //        navigator.goto_screen_mapEditor(0);
 
         // *********************** New Game Screen **********************
-        navigator.goto_screen_new_game_screen();
+//        navigator.goto_screen_new_game_screen();
 
         // *********************** IN GAME SCREEN *************************
         /**
