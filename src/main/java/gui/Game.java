4aa0e113fe2c41451c18b8c011e0d0087603f617
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-04-28 15:18:35 +0200

    add attributes for checkboxes

--- src/main/java/gui/Game.java
+++ src/main/java/gui/Game.java
@@ -57,6 +57,7 @@ public class Game extends JPanel implements Runnable {
     public final int gameOverState = 3;
     public ScreenNavigator screenNavigator;
     public long timeToFinish;
+    private boolean intelligent_monsters, advanced_powerups, hindering_curses;
 
     public Game(
             ScreenNavigator screenNavigator,
@@ -65,9 +66,9 @@ public class Game extends JPanel implements Runnable {
             boolean threePlayers,
             String player_name3,
             String mapID,
-            boolean p1AI,
-            boolean p2AI,
-            boolean p1Turn) {
+            boolean intelligent_monsters,
+            boolean advanced_powerups,
+            boolean hindering_curses) {
         this.screenNavigator = screenNavigator;
         this.gameMap = MapsController.getMapById(mapID);
 
@@ -84,6 +85,10 @@ public class Game extends JPanel implements Runnable {
         this.addKeyListener(keyHandler);
         this.setFocusable(true);
         this.setBackground(Slate._950);
+
+        this.intelligent_monsters = intelligent_monsters;
+        this.advanced_powerups = advanced_powerups;
+        this.hindering_curses = hindering_curses;
 
         this.playerNames.put("player1", player_name1);
         this.playerNames.put("player2", player_name2);
@@ -128,9 +133,11 @@ public class Game extends JPanel implements Runnable {
         obj.add(new ChestObject(new Point(3 * tileSize, 3 * tileSize), this));
 
         monsters.add(new BasicMonster(this, 1, new Point(11 * tileSize, tileSize)));
-        monsters.add(new GhostMonster(this, 2, new Point(7 * tileSize, 8 * tileSize)));
-        monsters.add(new ChasingMonster(this, players, 3, new Point(8 * tileSize, 9 * tileSize)));
-        monsters.add(new TipsyMonster(this, players, 4, new Point(tileSize, 10 * tileSize)));
+        if(intelligent_monsters){
+            monsters.add(new GhostMonster(this, 2, new Point(7 * tileSize, 8 * tileSize)));
+            monsters.add(new ChasingMonster(this, players, 3, new Point(8 * tileSize, 9 * tileSize)));
+            monsters.add(new TipsyMonster(this, players, 4, new Point(tileSize, 10 * tileSize)));
+        }
     }
 
     public void startGameThread() {
