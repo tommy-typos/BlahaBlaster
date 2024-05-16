323e0c27fdca377c81acf6a6beae3a7b444e4bb7
ToghrulMukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-16 07:18:30 +0200

    javadocs for mappreview & mappreview wrapper

--- src/main/java/gui/MapPreviewWrapper.java
+++ src/main/java/gui/MapPreviewWrapper.java
@@ -3,12 +3,24 @@ package gui;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * The MapPreviewWrapper class represents a panel that wraps a MapPreview component.
+ * It provides a method to set and display a MapPreview within it.
+ */
 class MapPreviewWrapper extends JPanel {
+  
+  /**
+   * Constructs a new MapPreviewWrapper panel.
+   */
   public MapPreviewWrapper() {
     super();
     this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
   }
 
+  /**
+   * Sets the MapPreview to be displayed within this wrapper panel.
+   * @param mapPreview The MapPreview component to be displayed.
+   */
   public void setMapPreview(MapPreview mapPreview) {
     this.removeAll();
     this.add(mapPreview);
